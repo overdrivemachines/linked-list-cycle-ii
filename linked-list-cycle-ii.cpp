e36e3bd90336ea04945aff605b98ba88951b69a7
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-03 20:13:00 -0700

    able to detect cycles

--- linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii.cpp
@@ -14,12 +14,17 @@ void printLinkedList(ListNode* head);
 ListNode* detectCycle(ListNode* head);
 
 int main(int argc, char const* argv[]) {
-  vector<int> v1 = {5, 32, 1, 2, 93, 75, 89};
-  vector<int> v2 = {3, 2, 0, -4};
+  vector<int> v1 = {5, 32, 1, 45, 93, 75, 89};
+  vector<int> v2 = {3, 68, 0, -4};
+  vector<int> v3 = {6, 95, -5, 0, 56};
 
   int pos1 = 3, pos2 = 1;
   ListNode* list1 = createLinkedList(v1, pos1);
   ListNode* list2 = createLinkedList(v2, pos2);
+  ListNode* list3 = createLinkedList(v3, -1);
+
+  ListNode* cycleNode = detectCycle(list3);
+  cout << "cycle node: " << cycleNode->val << endl;
 
   // printLinkedList(list1);
   // printLinkedList(list2);
@@ -28,7 +33,31 @@ int main(int argc, char const* argv[]) {
 }
 
 ListNode* detectCycle(ListNode* head) {
-  ListNode* node;
+  ListNode* node = head;  // traverse linked list
+  ListNode* nodeNext;
+  bool hasCycle = false;
+  int count = 0;
+
+  for (int i = 0; node != nullptr; i++, node = node->next) {
+    cout << node->val << "->";
+    nodeNext = node->next;
+    node = head;
+
+    for (int j = 0; j < i; j++) {
+      if (nodeNext == node) {
+        return node;
+      }
+      node = node->next;
+    }
+    // node = node->next;
+    if (i >= 4) {
+      exit(0);
+    }
+  }
+
+  if (node == nullptr) {
+    cout << "\nNo cycles found\n";
+  }
   return node;
 }
 
